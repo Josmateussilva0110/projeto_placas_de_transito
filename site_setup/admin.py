077a37e6c17b62ee5f955290b29812b650b312dc
Jose Mateus <jmateussilva044@gmail.com>
2024-07-25 19:45:12 -0300

    updates em site_setup

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from .models import Menu_Link, SiteSetup
 
-# Register your models here.
+@admin.register(Menu_Link)
+class AdminMenuLink(admin.ModelAdmin):
+    list_display = 'id', 'text', 'url_or_path',
+    list_display_links = 'id', 'text',
+    search_fields = 'id', 'text', 'url_or_path',
+
+
+class MenuLinkInline(admin.TabularInline):
+    model = Menu_Link
+    extra = 1
+
+@admin.register(SiteSetup)
+class AdminSiteSetup(admin.ModelAdmin):
+    list_display = 'title', 'description',
+    inlines = MenuLinkInline,
+
+
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
