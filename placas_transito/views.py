c9c0281119d654f2a29fa3597c32173cc98cbd91
Jose Mateus <jmateussilva044@gmail.com>
2024-07-26 09:18:30 -0300

    updades nos arquivos de html

--- placas_transito/views.py
+++ placas_transito/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+posts = list(range(1000))
 
 
 def index(request):
-    return render(request, 'placas_transito/pages/index.html')
+    context = dict()
+    pagination = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = pagination.get_page(page_number)
+    context['page_obj'] = page_obj
+    return render(request, 'placas_transito/pages/index.html', context)
 
 
 def page(request):
+    context = dict()
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(request, 'placas_transito/pages/page.html')
 
 
 def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(request, 'placas_transito/pages/post.html')
-
