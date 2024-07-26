663231acf43fb5f75b2c70cf32484b844607f155
Jose Mateus <jmateussilva044@gmail.com>
2024-07-25 22:09:08 -0300

    favicon de index completo

--- utils/images.py
+++ utils/images.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from django.conf import settings
 from PIL import Image
 
-
 def resize_image(image_django, new_width=800, optimize=True, quality=60):
-    image_path = Path(settings.MEDIA_ROOT / image_django.name).resolve()
+    # Converta settings.MEDIA_ROOT para um objeto Path
+    media_root_path = Path(settings.MEDIA_ROOT)
+    image_path = (media_root_path / image_django.name).resolve()
     image_pillow = Image.open(image_path)
     original_width, original_height = image_pillow.size
 
