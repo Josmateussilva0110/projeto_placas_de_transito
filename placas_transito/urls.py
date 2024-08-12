c9e2252212b17938f5760119e8f989a298120348
Jose Mateus <jmateussilva044@gmail.com>
2024-08-12 08:40:47 -0300

    pesquisa implementado

--- placas_transito/urls.py
+++ placas_transito/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('page/<slug:slug>', page, name='page'),
     path('post/<slug:slug>/', post, name='post'),
     path('category/<slug:slug>/', category, name='category'),
+    path('search/', search, name='search'),
 ]
--- placas_transito/views.py
+++ placas_transito/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from placas_transito.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -24,6 +25,15 @@ def category(request, slug):
     context['page_obj'] = page_obj
     return render(request, 'placas_transito/pages/index.html', context)
 
+
+def search(request):
+    context = dict()
+    search_value = request.GET.get('search', '').strip()
+    posts = Post.objects.get_published().filter(Q(title__icontains=search_value) | Q(excerpt__icontains=search_value) | Q(content__icontains=search_value))[:PER_PAGE]
+
+    context['page_obj'] = posts
+    context['search_value'] = search_value
+    return render(request, 'placas_transito/pages/index.html', context)
 
 def page(request, slug):
     context = dict()
